5cf8ea0bb792fe28d8c5417609f88f23262d1d7e
Romain GRANDJEAN <romaingrandjean@Romains-MacBook-Air.local>
2020-06-29 16:06:19 +0200

    version with basic graphic elements

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,9 +4,10 @@ class Nav extends React.Component {
     render() {
             return <div className="navigation">
                 <img src="./img/logo.png" alt=""/>
+                <span id="search">SEARCH</span><div className="whiteline"></div>
             </div>
 
     }
 }
 
-export default Nav;
+export default Nav; 
