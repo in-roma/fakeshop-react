4107dc00fe6935714c27b5d14fc745cfd22af60e
Romain GRANDJEAN <romaingrandjean@Romains-Air.fritz.box>
2020-07-01 22:30:03 +0200

    including menu buttons

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 class Cart extends React.Component {
     render() {
             return <div className="cart">
+                <img className="account-button" src="./img/accout-icon.svg"></img>
                 <img className="cart-button" src="./img/cart-icon.svg"></img>
             </div>
 
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,7 +4,9 @@ class Nav extends React.Component {
     render() {
             return <div className="navigation">
                 <img src="./img/logo.png" alt=""/>
-                {/* <span id="search">SEARCH</span><div className="whiteline"></div> */}
+                <span id="search">SEARCH</span>
+                <input type="text" className="search-input" placeholder=""></input>
+                <div className="white-line-search"></div>
             </div>
 
     }
